7695d4966945edc21338f40bac05c950bc680858
Ngoc Minh <minhdaihop@gmail.com>
2024-09-17 14:15:13 +0700

    add youtube

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    <h1>Hello World!</h1>
+    <p>Check out this video: <a href="https://www.youtube.com/" target="_blank">YouTube</a></p>
+  `);
 });
 
 app.listen(port, () => {
